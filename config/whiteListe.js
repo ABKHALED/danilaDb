83f210875f674306d909a36a4bb790ea418c014a
ABKHALED <khaledlakamora1995@gmqil.com>
2024-06-11 00:36:11 +0200

    ready to deploy backend

---
+++ config/whiteListe.js
@@ -0,0 +1,12 @@
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://www.dandrepairshop.com",
+  "https://dandrepairshop.com",
+  "localhost:3000",
+  "https://spher.netlify.app/",
+  "http://spher.netlify.app/",
+  "spher.netlify.app",
+  "spher.netlify.app/",
+];
+
+module.exports = allowedOrigins;
